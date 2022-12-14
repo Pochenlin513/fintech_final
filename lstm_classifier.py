0843ec22b0f8c1c9f5c374f7e43e2e10558060b8
pochen.lin <pochenlin0513@gmail.com>
2022-12-15 01:57:04 +0800

    update collection functions to remove NaNs

--- lstm_classifier.py
+++ lstm_classifier.py
@@ -6,22 +6,9 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 import torch.nn.functional as F
 from tqdm import tqdm
+from utils import score
 
 #%%
-def score(y_true: pd.DataFrame, y_pred: pd.DataFrame):
-    """ 
-    y_true - pandas.DataFrame: alert_key | sar_flag
-    y_pred - pandas.DataFrame: alert_key | prob  
-    """
-    n = len(y_true[y_true.sar_flag==1]) # Num sar
-    sar = y_true[y_true.sar_flag==1].alert_key.values # sar alert keys
-    hit = 0
-    for i, y_pred_ in enumerate(y_pred.alert_key.values):
-        if y_pred_ in sar:
-            hit += 1
-        if hit == n-1:
-            break
-    return (n-1)/(i+1), n-1, i
 
 class seqDataset(Dataset):
     def __init__(self, seq, label):
---
+++ utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+def score(y_true: pd.DataFrame, y_pred: pd.DataFrame):
+    """ 
+    y_true - pandas.DataFrame: alert_key | sar_flag
+    y_pred - pandas.DataFrame: alert_key | prob  
+    """
+    n = len(y_true[y_true.sar_flag==1]) # Num sar
+    sar = y_true[y_true.sar_flag==1].alert_key.values # sar alert keys
+    y_pred.sort_values(by=['prob'], ascending=False, inplace=True)
+    hit = 0
+    for i, y_pred_ in enumerate(y_pred.alert_key.values):
+        if y_pred_ in sar:
+            hit += 1
+        if hit == n-1:
+            break
+    return (n-1)/(i+1), n-1, i
+
+def create_dir(path):
+    while not os.path.isdir(path):
+        print('path not exist')
+        try:
+            os.mkdir(path)
+        except FileNotFoundError:
+            par_dir = '/'.join(path.split('/')[:-2]) + '/'
+            print(f'par_dir is {par_dir}')
+            create_dir(par_dir)
+        except FileExistsError:
+            pass
