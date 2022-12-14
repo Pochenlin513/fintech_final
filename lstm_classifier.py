e09035aadc8ba413cf48828e45966d961ad63d58
pochen.lin <pochenlin0513@gmail.com>
2022-12-14 17:08:45 +0800

    add time series statistics data

--- lstm_classifier.py
+++ lstm_classifier.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 import torch.nn.functional as F
+from tqdm import tqdm
 
 #%%
 def score(y_true: pd.DataFrame, y_pred: pd.DataFrame):
@@ -37,6 +38,11 @@ class seqDataset(Dataset):
 label = pd.read_csv('E:/Datasets/Fintech/TrainingDataset_first/train_y_answer.csv')
 data = pd.read_csv('./combined_data.csv')
 data.fillna(-1, inplace=True)   
+max_len = 0
+for ak in tqdm(data.alert_key.unique()):
+    if len(data[data.alert_key==ak]) > max_len:
+        max_len = len(data[data.alert_key==ak])
+        max_ak = ak
 # %%
 label = pd.read_csv('E:/Datasets/Fintech/TrainingDataset_first/train_y_answer.csv')
 pred = pd.DataFrame([], columns=['alert_key', 'probability'])
